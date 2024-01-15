0a9ce895b1c9691402523874768be159dd4eec12
aSwanting <gab.damico@gmail.com>
2024-01-15 17:59:25 +0100

    add: base api call/print in index.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <ProjectIndex />
+</template>
+
+<script>
+import ProjectIndex from "./pages/projects/Index.vue";
+export default {
+  components: {
+    ProjectIndex,
+  },
+};
+</script>
+
+<style lang="scss">
+@use "./style/general.scss";
+</style>
---
+++ src/pages/projects/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Projects</h1>
+    <div v-for="project in projects" :key="project.id">
+      {{ project.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      projects: [],
+      API_URL: "http://127.0.0.1:8000/api",
+    };
+  },
+  methods: {
+    fetchProjects() {
+      axios.get(`${this.API_URL}/projects`).then((res) => {
+        this.projects = res.data.results;
+      });
+    },
+  },
+  created() {
+    this.fetchProjects();
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
